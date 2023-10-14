bb1149de6b2d927e77bb03daf2956e8383a3738c
Manduru Raviteja <raviteja3720@gmail.com>
2023-10-14 11:24:37 +0530

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,25 +9,105 @@ export class AppComponent {
     personDetailsObj = new PersonalDetails();
     addressObj = new Address();
 
+    searchValue: any = '';
+
+    searchInputField: any = false;
+
+    searchPersonArray: any = [];
+
+    DisplaysearchPersonArray=false;
+
     multiplePersons: PersonalDetails[] = [];
 
     multipleAddress: Address[] = [];
 
-    personalDetailsDisplay: boolean = true;
+    SaveAddressBtn = true;
 
-    showAllDetails: boolean = false;
+    addAddressButton = true;
 
-    addressDisplay: boolean = false;
+    submitHide = true;
 
-    submitHide: boolean = true;
+    personalDetailsForm = false;
 
-    isUpdate: boolean = false;
+    TablePersonalDetailsDisplay = false;
 
-    back:boolean=false;
+    addressDisplayForm = false;
+
+    AddressDetailsDisplay = false;
+
+    TabledisplayAllAddress = false;
+
+    updatePersonbtn = false;
+
+    updateAddressbtn = false;
+
+    isUpdate = false;
+
+    saveBtn = false;
+
+    back = false;
+
+    home() {
+        this.personDetailsObj = new PersonalDetails();
+        this.personalDetailsForm = false;
+        this.TablePersonalDetailsDisplay = false;
+        this.personalDetailsForm = false;
+        this.addressDisplayForm = false;
+        this.AddressDetailsDisplay = false;
+        this.TabledisplayAllAddress = false;
+        this.updatePersonbtn = false;
+        this.updateAddressbtn = false;
+        this.isUpdate = false;
+        this.saveBtn = false;
+        this.back = false;
+        this.DisplaysearchPersonArray=false;
+        this.searchInputField=false;
+    }
+
+    DisplayAllUsers() {
+        this.TablePersonalDetailsDisplay = true;
+        this.personalDetailsForm = false;
+        this.addressDisplayForm = false;
+        this.AddressDetailsDisplay = false;
+        this.TabledisplayAllAddress = false;
+        this.updatePersonbtn = false;
+        this.updateAddressbtn = false;
+        this.isUpdate = false;
+        this.saveBtn = false;
+        this.back = false;
+        this.searchInputField = true;
+        this.DisplaysearchPersonArray=false;
+    }
+
+    AddPerson() {
+        this.personDetailsObj = new PersonalDetails();
+        this.personalDetailsForm = true;
+        this.addAddressButton = true;
+        this.saveBtn = true;
+        this.updatePersonbtn = false;
+        this.TablePersonalDetailsDisplay = false;
+        this.TablePersonalDetailsDisplay = false;
+        this.TablePersonalDetailsDisplay = false;
+        // this.addressDisplayForm = false;
+        this.AddressDetailsDisplay = false;
+        this.TabledisplayAllAddress = false;
+        this.updateAddressbtn = false;
+        this.isUpdate = false;
+        this.back = false;
+        this.DisplaysearchPersonArray=false;
+        this.searchInputField=false;
+    }
 
     addAddress() {
-        this.addressDisplay = true;
-        this.personalDetailsDisplay = false;
+        this.addressDisplayForm = true;
+        this.personalDetailsForm = true;
+        this.addAddressButton = false;
+        this.TablePersonalDetailsDisplay = false;
+        this.AddressDetailsDisplay = false;
+        this.TabledisplayAllAddress = false;
+        this.addressObj = new Address();
+        this.updateAddressbtn = false;
+        this.SaveAddressBtn = true;
     }
 
     additionalAddress() {
@@ -35,15 +115,44 @@ export class AppComponent {
         this.personDetailsObj.Addr.push(this.addressObj);
         this.addressObj = new Address();
         // console.log(this.arrayAddress.length);
+        this.TabledisplayAllAddress = true;
+        this.addressDisplayForm = false;
+        this.addAddressButton = true;
 
     }
 
-    addPersonalDetails() {
+    // addPersonalDetails() {
+    //     this.multiplePersons.push(this.personDetailsObj);
+    //     this.personDetailsObj.Addr.push(this.addressObj);//added
+    //     this.personalDetailsForm = false;
+    //     this.addressDisplayForm = false;
+    //     this.personDetailsObj = new PersonalDetails();
+    //     // console.log(this.multiplePersons.length);
+    //     this.TablePersonalDetailsDisplay = true;
+    //     this.AddressDetailsDisplay = true;
+    //     this.submitHide = false;
+    //     this.saveBtn=false
+    //     this.back=true;
+    // }
+
+    addPersonalDetails1() {
         this.multiplePersons.push(this.personDetailsObj);
-        this.personalDetailsDisplay = true;
-        this.addressDisplay = false;
+        console.log(this.multiplePersons.length);
+        // this.personDetailsObj.Addr.push(this.addressObj);
         this.personDetailsObj = new PersonalDetails();
-        // console.log(this.multiplePersons.length);
+        this.TabledisplayAllAddress = false;
+        this.TablePersonalDetailsDisplay = false;
+        this.personalDetailsForm = false;
+        this.addressDisplayForm = false;
+        this.saveBtn = false;
+        this.back = false;
+    }
+
+    cancelAddress() {
+        this.addressDisplayForm = false;
+        this.personalDetailsForm = true;
+        this.addAddressButton = true;
+        this.TabledisplayAllAddress = true;
     }
 
     deletePersonDetails(i: number) {
@@ -52,33 +161,105 @@ export class AppComponent {
 
     updatePerson(s: any) {
         this.personDetailsObj = s;
-        this.isUpdate=true;
-        this.showAllDetails = false;
-        this.personalDetailsDisplay = true;
+        this.isUpdate = true;
+        this.TablePersonalDetailsDisplay = false;
+        this.personalDetailsForm = true;
+        this.AddressDetailsDisplay = false;
     }
 
     deleteAddress(a: any, pId: any, aId: any) {
         this.multiplePersons[pId].Addr.splice(aId, 1);
     }
+    deleteAddress1(id: any) {
+        this.personDetailsObj.Addr.splice(id, 1);
+    }
 
-    updateAddress(a: any, pId: any, aId: any) {
-        this.isUpdate=true;
-        this.addressDisplay = true;
-        this.showAllDetails = false;
-        this.addressObj = a;
+    // updateAddress(a: any, pId: any, aId: any) {
+    //     this.isUpdate = true;
+    //     this.personalDetailsForm = false;
+    //     this.addressDisplayForm = true;
+    //     this.TablePersonalDetailsDisplay = false;
+    //     this.addressObj = a;
+    //     this.isUpdate = true;
+    // }
+
+    editAddress(addobj: any, Id: any) {
+        console.log(addobj + "----------" + Id);
+        this.addressDisplayForm = true;
+        this.TabledisplayAllAddress = false;
+        this.addressObj = addobj;
         this.isUpdate = true;
+        this.SaveAddressBtn = false;
+        this.updateAddressbtn = true
+        // this.personDetailsObj.Addr[Id]=addobj;
+    }
+
+    updateAddress1() {
+        this.personalDetailsForm = true;
+        this.TabledisplayAllAddress = true;
+        this.addressDisplayForm = false;
+    }
+
+    editPerson(perObj: any, Id: any) {
+        this.personalDetailsForm = true;
+        this.personDetailsObj = perObj;
+        // this.AddressDetailsDisplay=true;
+        this.TabledisplayAllAddress = true;
+        this.updatePersonbtn = true;
+        this.TablePersonalDetailsDisplay = false;
+        this.back = true;
+
+    }
+
+    updatePerson1() {
+        // this.personDetailsObj = s;
+        this.TabledisplayAllAddress = false;
+        this.TablePersonalDetailsDisplay = true;
+        this.updatePersonbtn = false;
+        this.personalDetailsForm = false;
+        // this.addressDisplayForm = false;
+        this.AddressDetailsDisplay = false;
+        this.TabledisplayAllAddress = false;
+        // this.updatePersonbtn = false;
+        this.updateAddressbtn = false;
+        this.isUpdate = false;
+        // this.saveBtn = false;
+        this.back = false;
     }
     show() {
-        this.addressDisplay = false;
-        this.showAllDetails=true;
-        this.personalDetailsDisplay=false;
-        this.back=true;
+        this.addressDisplayForm = false;
+        this.TablePersonalDetailsDisplay = true;
+        this.AddressDetailsDisplay = true;
+        this.personalDetailsForm = false;
+        this.back = true;
+    }
+
+    backtoform() {
+        this.TablePersonalDetailsDisplay = true;
+        this.personalDetailsForm = false;
+        this.back = false;
+        this.isUpdate = false;
+        this.addAddressButton = false;
+        this.TabledisplayAllAddress = false;
+        this.AddressDetailsDisplay = false;
+        this.saveBtn = false;
     }
     onSub() {
-        this.showAllDetails = true;
-        this.addressDisplay = false;
+        this.TablePersonalDetailsDisplay = true;
+        this.addressDisplayForm = false;
         this.submitHide = false;
-        this.personalDetailsDisplay = false;
+        this.personalDetailsForm = false;
+    }
+
+    searchFunction(sValue: any) {
+        this.DisplaysearchPersonArray=true;
+        this.TablePersonalDetailsDisplay=false;
+        this.searchPersonArray = this.multiplePersons.filter((searchValue) => {
+            if (sValue == searchValue.Firstname)
+                return this.searchPersonArray;
+            console.log(this.searchPersonArray);
+        }
+        )
     }
 }
 
@@ -95,6 +276,7 @@ export class PersonalDetails {
 }
 
 export class Address {
+    public AddressType!: string;
     public Addre!: string;
     public City!: string;
     public State!: string;
